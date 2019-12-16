2afdebe6f0ba98a1701abd19af763bece56a78d3
Dustin Newman <dustinnewman98@gmail.com>
2019-12-15 23:24:46 -0800

    font credit

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -13,7 +13,7 @@ export default class Layout extends React.Component {
         />
         <Header />
         {this.props.children}
-        <footer>Objectivity</footer>
+        <footer>This site uses the <a href="https://www.behance.net/gallery/60530395/Objectivity-Free-Font-Family">"Objectivity"</a> font family made by <a href="https://www.behance.net/slobzheninov">Alex Slobzheninov</a>.</footer>
       </div>
     );
   }
