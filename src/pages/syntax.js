8824b444e854d3191ef6e225551c59c042c9fe83
Dustin Newman <dustinnewman98@gmail.com>
2020-01-31 14:36:21 -0800

    syntax tree

--- src/pages/syntax.js
+++ src/pages/syntax.js
@@ -5,7 +5,7 @@ import Layout from "../layouts"
 
 export default () => (
     <Layout>
-        <SEO title="Syntax Tree Gen" description="Online syntax tree generator with bullet features" />
+        <SEO title="Syntax Tree Generator" description="Online syntax tree generator with bullet features and bracket notation." />
         <div style={{ marginTop: "2em", marginBottom: "2em" }}>
             <App />
         </div>
